b4b5c1d2fa4e700bc5b383fdc5490b7f3d7876a4
wadesweatt <wade.sweatt@gmail.com>
2013-05-10 09:51:40 -0400

    Minor Updates

---
+++ SimpleLayeredDrawer/RVGroupsTableView.h
@@ -0,0 +1,13 @@
+//
+//  RVGroupsTableView.h
+//  SimpleLayeredDrawer
+//
+//  Created by Wade Sweatt on 5/10/13.
+//  Copyright (c) 2013 J. Wade Sweatt. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RVGroupsTableView : NSTableView
+
+@end
---
+++ SimpleLayeredDrawer/RVGroupsTableView.m
@@ -0,0 +1,28 @@
+//
+//  RVGroupsTableView.m
+//  SimpleLayeredDrawer
+//
+//  Created by Wade Sweatt on 5/10/13.
+//  Copyright (c) 2013 J. Wade Sweatt. All rights reserved.
+//
+
+#import "RVGroupsTableView.h"
+
+@implementation RVGroupsTableView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
